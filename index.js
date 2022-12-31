e54eaff683f98f9b5c176e02d159125a666d6f9c
bhwk <120708160+bhwk@users.noreply.github.com>
2022-12-31 16:34:39 +0800

    Add functions to carry out operations

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let displayArea = document.querySelector('.display');
+let firstNumber = null;
+let secondNumber = null;
+let operatorSymbol = null;
+let result = null;
+
 function setupColours(){
     const buttons = document.querySelectorAll('button');
     for(let i=1; i<=buttons.length; i++){
@@ -14,10 +20,12 @@ function setupColours(){
     }
 
 }
+
 function addListeners(){
     const buttons = document.querySelectorAll('button');
     buttons.forEach((button)=>{
-        button.addEventListener('click', (e)=>console.log(e.target.textContent));
+        if(!isNaN(Number(button.textContent))) button.addEventListener('click', (e)=> updateDisplay(e.target.textContent));
+        else button.addEventListener('click', (button)=> operatorAssignment(button.target.textContent));
     });
 }
 
@@ -27,6 +35,109 @@ function addClasses(){
         if(!isNaN(Number(button.textContent))) button.classList.add('number');
         else button.classList.add('operator');
     })
+}
+
+function updateDisplay(number){
+    if (isNaN(Number(displayArea.textContent))) displayArea.textContent =0;
+    let displayNumber = displayArea.textContent;
+
+    if (!operatorSymbol){
+        if (displayNumber==result){
+            result = null;
+            displayNumber = number;
+        }
+        else if(displayNumber==0 && number ===0);
+
+        else if(displayNumber==0 && !displayNumber.includes('.')) displayNumber=number;
+
+        else displayNumber+=number;
+
+        displayArea.textContent= displayNumber;
+        firstNumber = Number(displayNumber);
+    }
+    else{
+        if (result!= null){
+            firstNumber = result;
+            result = null;
+        }
+        if (secondNumber === null) {
+            displayArea.textContent= 0;
+            displayNumber= displayArea.textContent;
+        }
+
+        if(displayNumber==0 && number ===0);
+
+        else if(displayNumber==0 && !displayNumber.includes('.')) displayNumber=number;
+
+        else displayNumber+=number;
+
+        displayArea.textContent=displayNumber;
+        secondNumber = Number(displayNumber);
+    }
+
+
+}
+
+function operatorAssignment(operator){
+    if (operator === '.' && !displayArea.textContent.includes('.')) displayArea.textContent+='.';
+    else if(operator === 'AC') {
+        displayArea.textContent=0;
+        firstNumber = null;
+        secondNumber = null;
+        operatorSymbol = null;
+        result = null;
+    }
+
+    else if(operator ==='=') {
+        if (!operatorSymbol);
+        else operation(firstNumber,secondNumber,operatorSymbol);
+    }
+
+    else if (operatorSymbol === operator && secondNumber === null){
+        operatorSymbol = null;
+    }
+
+    else if (operatorSymbol){
+        if (operator === '+' || operator ==='-' || operator === '/' || operator === '*'){
+            operation(firstNumber,secondNumber,operatorSymbol);
+            operatorSymbol = operator;
+        }
+    }
+
+    else {
+        operatorSymbol = operator;
+    }
+    
+}
+
+function operation(num1, num2, operator){
+    switch(true){
+        case(operator === '/' && num1 === 0 && num2 === 0):
+        displayArea.textContent = 'Zero Divison Error';
+        break;
+
+        case(operator === '+'):
+        result = num1 + num2;
+        break;
+        
+        case(operator ==='-'):
+        result = num1 - num2;
+        break;
+
+        case(operator ==='/'):
+        result = num1 / num2;
+        break;
+
+        case(operator ==='*'):
+        result = num1 * num2;
+        break;
+    }
+    result *= 1;
+    displayArea.textContent = result;
+    firstNumber = null;
+    secondNumber= null;
+    operatorSymbol = null;
+    console.log(num1,num2,operator);
 }
 
 setupColours();
